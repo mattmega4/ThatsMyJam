1e5e6f135eef9bd2cef3f0c98fc6d7af7de4bfb2
mattmega4 <mattmega4@gmail.com>
2018-02-02 14:51:24 -0800

    Initial Commit

---
+++ ThatsMyJam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThatsMyJam
+//
+//  Created by Matthew Howes Singleton on 2/2/18.
+//  Copyright © 2018 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
