e44965bcf027e1675ccbc1039249b8661eff2f8b
mattmega4 <mattmega4@gmail.com>
2018-03-14 22:17:45 -0700

    starting to add review code

---
+++ ThatsMyJam/Constants/ReviewKeys.swift
@@ -0,0 +1,14 @@
+//
+//  ReviewKeys.swift
+//  ThatsMyJam
+//
+//  Created by Matthew Howes Singleton on 3/14/18.
+//  Copyright © 2018 Matthew Howes Singleton. All rights reserved.
+//
+
+import Foundation
+
+struct FirebaseKeys {
+  static let runIncrementerSetting = "numberOfRuns"
+}
+
---
+++ ThatsMyJam/Model/RequestReview.swift
@@ -0,0 +1,50 @@
+//
+//  RequestReview.swift
+//  ThatsMyJam
+//
+//  Created by Matthew Howes Singleton on 3/14/18.
+//  Copyright © 2018 Matthew Howes Singleton. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+
+let runIncrementerSetting = "numberOfRuns"
+let minimumRunCount = 5
+
+
+func incrementAppRuns() {
+  let usD = UserDefaults()
+  let runs = getRunCounts() + 1
+
+  usD.setValuesForKeys([runIncrementerSetting: runs])
+  usD.synchronize()
+}
+
+
+func getRunCounts () -> Int {
+  let usD = UserDefaults()
+  let savedRuns = usD.value(forKey: runIncrementerSetting)
+  var runs = 0
+  if (savedRuns != nil) {
+    runs = savedRuns as! Int
+  }
+  print("Run Counts are \(runs)")
+  return runs
+}
+
+
+func showReview() {
+  let runs = getRunCounts()
+  if (runs > minimumRunCount) {
+    if #available(iOS 10.3, *) {
+      SKStoreReviewController.requestReview()
+    } else {
+      // Fallback on earlier versions
+    }
+  } else {
+    // Runs are not enough to request review
+  }
+}
+
